82cd3a04f2a023ec5af063c0432fddb26d705154
Martin Hagelin <martin@hagelin.nu>
2016-10-13 20:51:58 +0200

    Bump to 1.0.1

--- swish.php
+++ swish.php
@@ -33,7 +33,7 @@ class Swish extends PaymentModule
     {
         $this->name = 'swish';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'Martin Hagelin';
         $this->bootstrap = true;
         parent::__construct();
